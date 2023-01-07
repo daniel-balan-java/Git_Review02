ef13a9e9c356dfb7674bf172c4d24acf586d0525
Daniel Balan <114457618+daniel-balan-java@users.noreply.github.com>
2023-01-07 14:32:04 -0500

    Update FirstClass.java

--- src/first_package/FirstClass.java
+++ src/first_package/FirstClass.java
@@ -4,5 +4,8 @@ public class FirstClass {
     public static void main(String[] args) {
 
         System.out.println("First Class");
+        // Some changes 
+        System.out.println("First Class");
+
     }
 }
